455f5ccd674b1445a9bf3d12483aaf69a5e43d81
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2020-03-23 13:34:59 +0200

    [nest] - frontend basic logic implementation

--- nest/blog-frontend/src/index.tsx
+++ nest/blog-frontend/src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "./contexts/auth0-context";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
